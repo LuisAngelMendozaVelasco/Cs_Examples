2c8e3928f51331c04b66f532b1ccb2a46cbabd10
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 20:22:14 +0200

    Variables - Example 1

---
+++ Variables/Example_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a string variable
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            
+            Console.WriteLine(name);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);             
+        }
+    }
+}
