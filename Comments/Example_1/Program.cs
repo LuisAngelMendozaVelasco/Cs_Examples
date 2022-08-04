ab939cf0851e03c19517a5a327d352ebb80d0d39
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 18:50:39 +0200

    Comments - Example 1

---
+++ Comments/Example_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Single-line comment before a line of code
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            Console.WriteLine("Hello World!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);     
+        }
+    }
+}
