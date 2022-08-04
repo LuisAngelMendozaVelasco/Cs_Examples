950f994124dfe7dd3aeb1e760e33e99eb93f15ca
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 22:11:28 +0200

    DataTypes - Example 8

---
+++ DataTypes/Example_8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a string type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World";
+
+            Console.WriteLine(greeting);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
