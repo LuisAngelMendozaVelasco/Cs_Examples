b270a89464ee6adac08f2b45de0685d27c635007
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 20:22:26 +0200

    Variables - Example 2

---
+++ Variables/Example_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create an integer variable
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 15;
+            
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);       
+        }
+    }
+}
