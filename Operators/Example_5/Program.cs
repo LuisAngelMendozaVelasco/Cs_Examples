b1115225fabbf00b5154e24641ea94779c2f550f
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:05:52 +0200

    Operators - Example 5

---
+++ Operators/Example_5/Program.cs
@@ -0,0 +1,19 @@
+﻿//Modulus operator
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+
+            Console.WriteLine(x % y);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
