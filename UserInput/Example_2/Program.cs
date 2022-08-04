be95844c41ac373eedaba09e45472fa98d978867
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 00:27:53 +0200

    UserInput - Example 2

---
+++ UserInput/Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Get user input with numbers
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nYour age is: " + age);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
