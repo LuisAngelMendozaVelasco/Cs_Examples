8a5c8fa91c8ef30156b7f2c0c35c274aadf12f53
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:49:08 +0200

    Math - Example 4

---
+++ Math/Example_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Math.Abs(x) - return the absolute (positive) value of x
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-4.7));  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
