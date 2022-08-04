41c4f2df36fca85f93877f3604e659d76ccee190
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:31:15 +0200

    Variables - Example 6

---
+++ Variables/Example_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Add a variable to another variable
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Luis ";
+            string lastName = "Mendoza";
+            string fullName = firstName + lastName;
+
+            Console.WriteLine(fullName);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
