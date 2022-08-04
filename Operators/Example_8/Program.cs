e63d1ca32198f10339e51490e883ad899c1503e1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:13:03 +0200

    Operators - Example 8

---
+++ Operators/Example_8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Assigment operator
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            Console.WriteLine(x);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
