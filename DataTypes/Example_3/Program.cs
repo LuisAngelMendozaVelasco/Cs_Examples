ee50bdd41b74ae9dec956a16b3af22ed3ad689bd
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:55:26 +0200

    DataTypes - Example 3

---
+++ DataTypes/Example_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a long type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myNum = 15000000000L;
+
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
