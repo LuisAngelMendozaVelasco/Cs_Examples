44e80c80bbe4eeadafcb8131b17dfd2c71e60b1f
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 00:57:32 +0200

    Operators - Example 2

---
+++ Operators/Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Subtraction operator
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            
+            Console.WriteLine(x - y); 
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
