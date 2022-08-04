906e9ead970aae4ab23d54cf5f98a69982046c4c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:52:52 +0200

    DataTypes - Example 2

---
+++ DataTypes/Example_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create an int type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 100000;
+
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
