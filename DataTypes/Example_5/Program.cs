4622503877c68003d97aad9bc93ef342acb3b2d1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 22:04:19 +0200

    DataTypes - Example 5

---
+++ DataTypes/Example_5/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a double type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myNum = 19.99D;
+            
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
