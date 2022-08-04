4f92805ef810604f9f757fb34c22d901de7f5579
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:51:05 +0200

    Math - Example 5

---
+++ Math/Example_5/Program.cs
@@ -0,0 +1,16 @@
+﻿//Math.Round() - round a number to the nearest whole number
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(9.99));
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
