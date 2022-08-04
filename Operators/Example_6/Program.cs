ec9fca7812803064c1c247f3621a25854197f4f1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:08:00 +0200

    Operators - Example 6

---
+++ Operators/Example_6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Increment operator
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+
+            x++;
+            Console.WriteLine(x);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
