ccce6c360262665c96a1df1a340f265a32c597bb
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 22:07:43 +0200

    DataTypes - Example 6

---
+++ DataTypes/Example_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a bool type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+
+            Console.WriteLine(isCSharpFun);   // Outputs True
+            Console.WriteLine(isFishTasty);   // Outputs False 
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);   
+        }
+    }
+}
