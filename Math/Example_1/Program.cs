4e8f62803330f123a90cd10b7c75a60e5881239a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:43:22 +0200

    Math - Example 1

---
+++ Math/Example_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Math.Max(x,y) - return the highest value of x and y
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5, 10));  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
