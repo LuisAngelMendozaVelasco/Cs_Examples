09edb34151c5165c711e05ed7675d1686f48ec89
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:15:28 +0200

    Operators - Example 9

---
+++ Operators/Example_9/Program.cs
@@ -0,0 +1,19 @@
+﻿//Addition assigment operator
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            
+            x += 5;
+            Console.WriteLine(x);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
