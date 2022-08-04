f846a8aa7dcac3103d06e45c291a5c0a5d2e5ab3
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:33:36 +0200

    Variables - Example 7

---
+++ Variables/Example_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Declare many variables of the same type with a comma-separated list
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 6, z = 50;
+
+            Console.WriteLine(x + y + z);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
