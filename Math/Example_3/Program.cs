85d679b87e1401d74e0c5edd63b6a0ef1087afe0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:47:27 +0200

    Math - Example 3

---
+++ Math/Example_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Math.Sqrt(x) - return the square root of x
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(64));  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
