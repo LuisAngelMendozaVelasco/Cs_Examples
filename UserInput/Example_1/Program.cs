d9eec53b618a3be14996642cdda2af5bd2c946ea
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 00:09:41 +0200

    UserInput - Example 1

---
+++ UserInput/Example_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Get user input text
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type your username and press enter
+            Console.WriteLine("Enter username:");
+
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("\nUsername is: " + userName);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
