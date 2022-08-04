29cfafed34d564faeb27cac788db331d319cecbe
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:22:55 +0200

    Variables - Example 4

---
+++ Variables/Example_4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Overwrite an existing variable value
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 15;
+            
+            myNum = 20;
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
