5f47446cf7c6f4b3380ad716e1be5d3211fa984d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:45:27 +0200

    DataTypes - Example 1

---
+++ DataTypes/Example_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//A demonstration of different data types in C#
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5;               // integer (whole number)
+            double myDoubleNum = 5.99D;  // floating point number
+            char myLetter = 'D';         // character
+            bool myBool = true;          // boolean
+            string myText = "Hello";     // string
+            
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
