d63b19de5bcf0f7c00c3c311a01dc79f9d3b70c2
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 18:59:25 +0200

    Comments - Example 3

---
+++ Comments/Example_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Multi-line comment
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* The code below will print the words Hello World
+            to the screen, and it is amazing */
+            Console.WriteLine("Hello World!");  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);    
+        }
+    }
+}
