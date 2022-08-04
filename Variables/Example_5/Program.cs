961b4ebc90f98f384cd93fd0374707e146bed0a6
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 21:27:19 +0200

    Variables - Example 5

---
+++ Variables/Example_5/Program.cs
@@ -0,0 +1,18 @@
+﻿//Combine text and a variable on display
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Luis";
+
+            Console.WriteLine("Hello " + name);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
