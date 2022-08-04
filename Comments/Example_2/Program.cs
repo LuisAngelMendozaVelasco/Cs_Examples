a8f89bffe788d71e5a55b2f4679aa778cf69afa1
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 18:55:43 +0200

    Comments - Example 2

---
+++ Comments/Example_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Single-line comment at the end of a line of code
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  // This is a comment   
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);    
+        }
+    }
+}
