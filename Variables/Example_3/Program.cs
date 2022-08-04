5fcdf3100d4685b8fec95d7a1076334b3e78125d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 20:23:49 +0200

    Variables - Example 3

---
+++ Variables/Example_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Create a variable without assigning the value, and assign the value later
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum;
+            
+            myNum = 15;
+            Console.WriteLine(myNum);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);  
+        }
+    }
+}
