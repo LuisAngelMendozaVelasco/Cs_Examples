560e96247dfcf74ecb1ba284595b73b30879be17
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-04 22:09:49 +0200

    DataTypes - Example 7

---
+++ DataTypes/Example_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a char type
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myGrade = 'B';
+
+            Console.WriteLine(myGrade);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
