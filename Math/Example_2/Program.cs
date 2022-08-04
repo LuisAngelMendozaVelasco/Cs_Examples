69cfe3f59cb6e0d0f6a636897be4f7fea932d46f
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 01:44:46 +0200

    Math - Example 2

---
+++ Math/Example_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Math.Min(x,y) - return the lowest value of x and y
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Min(5, 10));  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
