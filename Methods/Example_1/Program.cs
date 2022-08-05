acb84a7e3506a9f6f400dc3d356afdc34c67cbd5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:46:07 +0200

    Methods - Example 1

---
+++ Methods/Example_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create and call a method
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
