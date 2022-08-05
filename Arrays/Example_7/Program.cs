206230415ca8fcacf8e0dc1179a1bb109e52195d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:35:31 +0200

    Arrays - Example 7

---
+++ Arrays/Example_7/Program.cs
@@ -0,0 +1,35 @@
+﻿//Sort an array
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sort a string
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            Array.Sort(cars);
+            foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }    
+            
+            Console.Write($"{Environment.NewLine}");
+
+            // Sort an int
+            int[] myNumbers = {5, 1, 8, 9};
+
+            Array.Sort(myNumbers);
+            foreach(int i in myNumbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
