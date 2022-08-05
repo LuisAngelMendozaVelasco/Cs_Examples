672559c90400c9c79a231cf11d1150b67ce479e0
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:24:28 +0200

    Strings - Example 3

---
+++ Strings/Example_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Using methods to convert strings to uppercase and lowercase
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World";
+
+            Console.WriteLine(txt.ToUpper());   // Outputs "HELLO WORLD"
+            Console.WriteLine(txt.ToLower());   // Outputs "hello world"
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
