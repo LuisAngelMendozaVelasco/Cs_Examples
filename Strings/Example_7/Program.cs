f2c56d86c02c41a229fda7f7310151743bcff54a
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:45:56 +0200

    Strings - Example 7

---
+++ Strings/Example_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Access characters in a string by referring to its index number
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello";
+
+            Console.WriteLine(myString[0]);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
