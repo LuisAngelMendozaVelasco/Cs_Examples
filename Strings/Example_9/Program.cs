d94116469b7d3ced0036f8e12b9bf135fcd4054e
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:51:41 +0200

    Strings - Example 9

---
+++ Strings/Example_9/Program.cs
@@ -0,0 +1,26 @@
+﻿//Use the Substring() method together with the IndexOf() method
+
+using System;
+
+namespace GetLastName
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Full name
+            string name = "Luis Mendoza";
+
+            // Location of the letter D
+            int charPos = name.IndexOf("M");
+
+            // Get last name
+            string lastName = name.Substring(charPos);
+
+            // Print the result
+            Console.WriteLine(lastName);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
