2bc96baea5025d9c9048604bcc0130d0ad73f243
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 21:48:21 +0200

    ClassesObjects - Example 1

---
+++ ClassesObjects/Example_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a class and an object of a class
+
+using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car(); // To create an object of Car, specify the class name, followed by the object name, and use the keyword new
+
+            Console.WriteLine(myObj.color);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
