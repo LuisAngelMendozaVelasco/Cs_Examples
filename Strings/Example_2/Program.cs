52033b6302b1f60b0ef2f7f83bc1224f5af5350d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:20:52 +0200

    Strings - Example 2

---
+++ Strings/Example_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Find the length of a string
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
