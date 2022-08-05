5476ddd973c31b11a24f3280b026062843f66f26
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 21:08:24 +0200

    Methods - Example 3

---
+++ Methods/Example_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Method with parameters
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod(string fname) 
+        {
+            Console.WriteLine(fname + " Refsnes");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Liam");
+            MyMethod("Jenny");
+            MyMethod("Anja");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }  
+    }
+}
