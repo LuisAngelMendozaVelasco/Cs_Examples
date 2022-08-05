becee67c56d530fe075f342401f32c74320a64ad
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:41:59 +0200

    Arrays - Example 8

---
+++ Arrays/Example_8/Program.cs
@@ -0,0 +1,21 @@
+﻿//Using System.Linq
+
+using System;
+using System.Linq;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = {5, 1, 8, 9};
+
+            Console.WriteLine(myNumbers.Max());  // largest value
+            Console.WriteLine(myNumbers.Min());  // smallest value
+            Console.WriteLine(myNumbers.Sum());  // sum of myNumbers
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
