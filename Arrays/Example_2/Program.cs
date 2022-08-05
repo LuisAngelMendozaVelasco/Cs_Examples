18263df5f5d8d2b2e8de7e415ee620d54dff11ff
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:12:50 +0200

    Arrays - Example 2

---
+++ Arrays/Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Change an array element
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            cars[0] = "Opel";
+            Console.WriteLine(cars[0]);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }  
+    }
+}
