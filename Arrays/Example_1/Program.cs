8add3491e5fb9032d61c3370b6502b4a59586a97
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 18:37:33 +0200

    Arrays - Example 1

---
+++ Arrays/Example_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create and access an array
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            Console.WriteLine(cars[0]);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
