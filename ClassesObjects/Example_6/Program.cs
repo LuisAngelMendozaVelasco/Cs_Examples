010d506c458ea0cd79e81c7d2b7b07127d599dae
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 22:32:25 +0200

    ClassesObjects - Example 6

---
+++ ClassesObjects/Example_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Private modifier
+
+using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        private string model = "Mustang"; // The code is only accessible within the same class
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+
+            Console.WriteLine(myObj.model);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
