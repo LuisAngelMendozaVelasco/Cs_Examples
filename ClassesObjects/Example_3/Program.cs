f0d584c8569da0ffafb606546d4fd9f7ef678165
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 22:03:52 +0200

    ClassesObjects - Example 3

---
+++ ClassesObjects/Example_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Use multiple classes for better organization
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            
+            Console.WriteLine(myObj.color);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
