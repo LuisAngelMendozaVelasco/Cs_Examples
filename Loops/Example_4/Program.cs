7382cdc6ad73a31818dece27a534aafe0d8e2ae5
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 18:22:45 +0200

    Loops - Example 4

---
+++ Loops/Example_4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Foreach loop
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"}; // Declare a single-dimensional
+
+            foreach(string i in cars) 
+            {
+                Console.WriteLine(i);
+            }   
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
