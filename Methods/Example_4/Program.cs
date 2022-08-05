74eb6ef090d5377b393eb96f64c64aa98431d795
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 21:09:56 +0200

    Methods - Example 4

---
+++ Methods/Example_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Default parameter value
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod(string country = "Norway")
+        {
+            Console.WriteLine(country);
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod("Sweden");
+            MyMethod("India");
+            MyMethod();
+            MyMethod("USA");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
