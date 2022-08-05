3185a592d326028b7e1425b24d269c8b40769170
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 18:09:06 +0200

    Loops - Example 1

---
+++ Loops/Example_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//While loop
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while(i < 5) 
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
