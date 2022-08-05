f04810e54d1973a5108a7639118e3110cb9174ba
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 23:53:24 +0200

    Exceptions - Example 2

---
+++ Exceptions/Example_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// The finally statement
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = {1, 2, 3};
+
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }  
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
