d4d6dfba4f10005bd469eab0c434a2265334f57d
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 18:05:30 +0200

    Switch - Example 2

---
+++ Switch/Example_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//The switch statement with a default keyword
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+
+            switch(day) 
+            {
+                case 6:
+                    Console.WriteLine("Today is Saturday.");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday.");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+            }
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);    
+        }
+    }
+}
