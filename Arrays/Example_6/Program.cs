535131f50864f46fa7bb1a0a76b9fa4e1fd827fc
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:28:36 +0200

    Arrays - Example 6

---
+++ Arrays/Example_6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Loop through an array with foreach
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            foreach(string i in cars) 
+            {
+                Console.WriteLine(i);
+            }   
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
