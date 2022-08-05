f4461c006f9eb90abacb16d52e41bd96319c2df4
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 03:06:13 +0200

    Booleans - Example 2

---
+++ Booleans/Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Find out if an expression is true or false
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 9;
+            
+            Console.WriteLine(x > y); // returns True, because 10 is higher than 9
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
