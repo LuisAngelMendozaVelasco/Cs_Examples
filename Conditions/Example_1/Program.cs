aa7d842b64cab41e362be33af0661174dea70235
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 03:15:33 +0200

    Conditions - Example 1

---
+++ Conditions/Example_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//The if statement
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(20 > 18) 
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);    
+        }
+    }
+}
