5d11184bb937aee8dda4274dc84d17b818988b4b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:59:21 +0200

    Strings - Example 10

---
+++ Strings/Example_10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Use quotes in a string
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "We are the so-called \"Vikings\" from the north.";
+
+            Console.WriteLine(txt);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
