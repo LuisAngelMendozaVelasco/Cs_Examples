63f2d8adfb70153eaa2124ec64fc492cc4e0eb27
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 23:50:48 +0200

    Exceptions - Example 1

---
+++ Exceptions/Example_1/Program.cs
@@ -0,0 +1,25 @@
+﻿//The try..catch statement
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = {1, 2, 3};
+
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);    
+        }
+    }
+}
