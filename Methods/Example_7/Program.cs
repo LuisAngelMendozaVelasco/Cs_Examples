e21dfc97b31257375390d88027e089584faa78aa
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 21:29:07 +0200

    Methods - Example 7

---
+++ Methods/Example_7/Program.cs
@@ -0,0 +1,23 @@
+﻿//Return the sum of a method's two parameters
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int z = MyMethod(5, 3);
+
+            Console.WriteLine(z);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
