64832da46ae396c95c168c1e40f94ae561ee7c2b
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:42:32 +0200

    Strings - Example 6

---
+++ Strings/Example_6/Program.cs
@@ -0,0 +1,20 @@
+﻿//String interpolation
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Luis";
+            string lastName = "Mendoza";
+            string name = $"My full name is: {firstName} {lastName}";
+
+            Console.WriteLine(name);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
