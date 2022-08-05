269cf2da0ddefea5aff8c351221c186bf8a81b89
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 18:16:28 +0200

    Loops - Example 3

---
+++ Loops/Example_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//For loop
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine(i);
+            }    
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
