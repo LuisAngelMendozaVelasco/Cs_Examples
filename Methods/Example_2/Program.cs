ff2ab5848605dd7951edd96d8dfea5097e9fefec
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 20:49:08 +0200

    Methods - Example 2

---
+++ Methods/Example_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Call a method multiple times
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
