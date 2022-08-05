3f3c79e4fa56d06d2866b49c9b5dacc171dc520c
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 21:53:34 +0200

    ClassesObjects - Example 2

---
+++ ClassesObjects/Example_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create multiple objects of a class
+
+using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            Car myObj2 = new Car();
+
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
