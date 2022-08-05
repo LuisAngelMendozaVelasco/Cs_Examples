bda690204576796c5020114dae3d3d2682fa75ac
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:17:10 +0200

    Strings - Example 1

---
+++ Strings/Example_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a string
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+
+            Console.WriteLine(greeting);  
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
