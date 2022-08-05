1fee19714477c54203ca9cce3eca68f60e16fb07
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 03:32:37 +0200

    Conditions - Example 3

---
+++ Conditions/Example_3/Program.cs
@@ -0,0 +1,29 @@
+﻿//The else if statement
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 22;
+
+            if (time < 10) 
+            {
+                Console.WriteLine("Good morning.");
+            } 
+            else if (time < 20) 
+            {
+                Console.WriteLine("Good day.");
+            } 
+            else 
+            {
+                Console.WriteLine("Good evening.");
+            }
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);  
+        }
+    }
+}
