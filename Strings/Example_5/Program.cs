708878a9ba5415041bac072df564578830910013
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:31:37 +0200

    Strings - Example 5

---
+++ Strings/Example_5/Program.cs
@@ -0,0 +1,20 @@
+﻿//String concatenation with the Concat() method
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Luis ";
+            string lastName = "Mendoza";
+            string name = string.Concat(firstName, lastName);
+
+            Console.WriteLine(name);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
