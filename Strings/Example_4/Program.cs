af4c41222069d0a69583035794b9be75f800c891
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:28:02 +0200

    Strings - Example 4

---
+++ Strings/Example_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//String concatenation
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Luis ";
+            string lastName = "Mendoza";
+            string name = firstName + lastName;
+
+            Console.WriteLine(name);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
