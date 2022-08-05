1e4f1f4d28a3267dcaeca37b0f1f409b4585a1f7
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 19:14:40 +0200

    Arrays - Example 3

---
+++ Arrays/Example_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Find the length of an array
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            Console.WriteLine(cars.Length);
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true); 
+        }
+    }
+}
