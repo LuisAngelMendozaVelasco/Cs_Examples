bce6d0f5dcc1a85cce1d1f50351b6146a9c504d6
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 02:48:13 +0200

    Strings - Example 8

---
+++ Strings/Example_8/Program.cs
@@ -0,0 +1,18 @@
+﻿//Find the index position of a specific character in a string, by using the IndexOf() method
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello";
+
+            Console.WriteLine(myString.IndexOf("e"));
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
