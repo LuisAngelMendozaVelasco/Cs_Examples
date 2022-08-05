b89171bf6db45296755fe71df906d9b0219f8371
LuisAngelMendozaVelasco <luisangel.mendozavelasco@outlook.com>
2022-08-05 03:11:30 +0200

    Booleans - Example 3

---
+++ Booleans/Example_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Use the "equal to" operator to evaluate a boolean expression
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            
+            Console.WriteLine(x == 10); // returns True, because the value of x is equal to 10
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
